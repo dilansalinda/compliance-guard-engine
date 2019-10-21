0b0ced52ce12f70b4c1778005c9150ae667d6427
dsalinda <dsalinda@virtusa.com>
2019-10-21 15:03:47 +0530

    initial commit

---
+++ module02/src/main/java/com/dilan/module02/test2.java
@@ -0,0 +1,7 @@
+package com.dilan.module02;
+
+/**
+ * Created by dsalinda on 10/21/2019
+ */
+public class test2 {
+}
---
+++ src/main/java/com/dilan/module01/Test01.java
@@ -0,0 +1,13 @@
+package com.dilan.module01;
+
+/**
+ * Created by dsalinda on 10/21/2019
+ */
+public class Test01 {
+
+    public static void main(String[] args) {
+        if ("test" == "test") {
+            System.out.println("test ok");
+        }
+    }
+}
