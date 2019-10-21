e2276348a136a5e58dcdb0820e19164aa0143755
dsalinda <dsalinda@virtusa.com>
2019-10-21 17:31:00 +0530

    delete old

--- test/module01/src/main/java/com/dilan/module01/test1.java
+++
@@ -1,7 +0,0 @@
-package com.dilan.module01;
-
-/**
- * Created by dsalinda on 10/21/2019
- */
-public class test1 {
-}
--- test/module02/src/main/java/com/dilan/module02/test2.java
+++
@@ -1,7 +0,0 @@
-package com.dilan.module02;
-
-/**
- * Created by dsalinda on 10/21/2019
- */
-public class test2 {
-}
