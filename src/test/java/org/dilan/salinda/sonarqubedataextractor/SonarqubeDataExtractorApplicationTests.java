33940658eb5fe04c5643f9c675781550c86ea3c9
dilansalinda <dilansalinda@gmil.com>
2023-09-26 16:26:49 +0530

    add test case

--- src/test/java/org/dilan/salinda/sonarqubedataextractor/SonarqubeDataExtractorApplicationTests.java
+++ src/test/java/org/dilan/salinda/sonarqubedataextractor/SonarqubeDataExtractorApplicationTests.java
@@ -2,12 +2,14 @@ package org.dilan.salinda.sonarqubedataextractor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class SonarqubeDataExtractorApplicationTests {
 
     @Test
     void contextLoads() {
+        Assert.hasText("d", "d");
     }
 
 }
