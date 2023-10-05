16001b75cbca5a8418be40811b72b4c7424914b7
dilansalinda <dilansalinda@gmil.com>
2023-10-05 11:25:32 +0530

    remove unnecessary files

--- file/src/JavaSample.java
+++
@@ -1,9 +0,0 @@
-public class JavaSample {
-
- public static void main(String[] args) {
-	 
-	 private String s = null;
-	 private String ss = null;
-	 private String ss = null;
- }
-}
--- file/src/test.java
+++
@@ -1,7 +0,0 @@
-public class JavaSample {
-
- public static void main(String[] args) {
-	 
-	 private String s = null;
- }
-}
